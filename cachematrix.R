95f202a9275cdea7a51d1f0b216fcd386dae9eb4
Devinaseeruttun <bhookund@uom.ac.mu>
2014-06-19 20:23:21 +0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,36 @@
 message ( " #calling makecachematrix() function")
-message ( " # return value ( a list of four functions) to a variable,m")
-message ( " # m is now a list of four functions")
+message ( " # return value ( a list of four functions) to a variable, m")
+message ( " # m is now a list of four functions") 
 makeCacheMatrix <- function (x = matrix()){
       I <- NULL
  message ( "# use v's set function to create a matrix") 
- message ( " # with nrow =2 and nrow =2")
+ message ( " # with nrow =n and nrow =n")
       set <- function(y = matrix()){
         x <<- y
         I <<- NULL
 
 message ( "# use v's get function to retrieve the matrix created")  
 } 
-{
-  message ( "# pass the list v to the cachesolve() function")  
-  message ( " # the inverve  of a 2x2 matrix should be returned")
-}
        get <- function() x
        setsolve <- function(solve) I <<- solve(x)
        getsolve <- function() I
-
-  
+ 
   result <- list(set = set, get = get ,
                  setsolve = setsolve ,
                  getsolve = getsolve)
   return(result)
-  
-
 }  
 cacheSolve <- function(x) {  
+  
   I <- x$getsolve()
   if(!is.null(I)) { 
-  message("retrieving catched inverse") 
+  message("retrieving catched inverse matrix") 
     return(I)
   }
+  
   data <- x$get()
-  I <- solve(data, ...)
+  I <- solve(data)
   x$setsolve(I)
   I
-  message ("#use v's set function to create a new 3 x 3 matrix")
-
+  
 }
