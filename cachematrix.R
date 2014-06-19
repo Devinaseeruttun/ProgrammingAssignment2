0c217f72fc3c97fa3ea40d90fde9c08cf4f44e0d
Devinaseeruttun <bhookund@uom.ac.mu>
2014-06-19 19:43:18 +0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+message ( " #calling makecachematrix() function")
+message ( " # return value ( a list of four functions) to a variable,m")
+message ( " # m is now a list of four functions")
+makeCacheMatrix <- function (x = matrix()){
+      I <- NULL
+ message ( "# use v's set function to create a matrix") 
+ message ( " # with nrow =2 and nrow =2")
+      set <- function(y = matrix()){
+        x <<- y
+        I <<- NULL
 
-## Write a short comment describing this function
+message ( "# use v's get function to retrieve the matrix created")  
+} 
+{
+  message ( "# pass the list v to the cachesolve() function")  
+  message ( " # the inverve  of a 2x2 matrix should be returned")
+}
+       get <- function() x
+       setsolve <- function(solve) I <<- solve(x)
+       getsolve <- function() I
 
-makeCacheMatrix <- function(x = matrix()) {
+  
+  result <- list(set = set, get = get ,
+                 setsolve = setsolve ,
+                 getsolve = getsolve)
+  return(result)
+  
+
+}  
+cacheSolve <- function(x) {
+  message("#Pass the list m to the cachesolve a second time")
+  message("# the inverse of the 2 x 2 matrix should be returned")
+  message("# also a message retrieving catched invverse indicating that")
+  message("# the inverse matrix is not been calculated this time but ")
+  message("# is being retrieved from catched value")
+  message("retrieving catched inverse")  
+  I <- x$getsolve()
+  if(!is.null(I)) {
+    return(I)
+  }
+  data <- x$get()
+  I <- solve(data, ...)
+  x$setsolve(I)
+  I
+  message ("#use v's set function to create a new 3 x 3 matrix")
 
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    
